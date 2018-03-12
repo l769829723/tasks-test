bc4ed6ad9ee62109b0a38344cc1668506c646cfc
opsland <1054473703@qq.com>
2018-03-12 15:54:43 +0800

    添加注释，以及更新help方法，用来显示选项的帮助内容。

--- manage.py
+++ manage.py
@@ -13,7 +13,13 @@ from main.models import MODEL_TABLES
 
 
 class Management(object):
+  """
+  简易的应用控制工具，提供快捷创建数据库，和创建用户的方式。
+  """
   def __init__(self):
+    '''
+    初始化方法，检查当前路径是否在系统搜索路径中，如果不存在则添加。
+    '''
     super(Management, self).__init__()
     self._path = os.path.dirname(__file__)
     if self._path not in sys.path:
@@ -21,13 +27,16 @@ class Management(object):
   
   def __str__(self):
     return '''
-    Application management tool v.0.1.
+    应用程序管理控制台v0.1
     '''
 
   def runserver(self, address='127.0.0.1', port=5000):
     app.run(address, port)
 
   def initdb(self):
+    '''
+    根据定义的数据模型，创建数据表结构，该操作会清楚用户保存的已有数据。
+    '''
     try:
       DATABASE.connect()
       for table in MODEL_TABLES:
@@ -38,6 +47,11 @@ class Management(object):
       print(e)
 
   def useradd(self, username, password):
+    '''
+    添加一个用户，如果用户存在则更新用户名和密码。
+    :username 要添加或更新的用户名称
+    :password 用户要设置或者更新的密码
+    '''
     user = User.select().where(User.username == username).first()
     if user:
       user.password = password
@@ -48,6 +62,13 @@ class Management(object):
       user.role = 'ADMIN'
       user.locked = False
     user.save()
+
+  def help(self, option):
+    try:
+      doc = getattr(self, option).__doc__
+      print('Usage:\t\t{}\n{}'.format(option, doc.lstrip()))
+    except AttributeError:
+      print('Invalid option.')
 
 
 if __name__ == '__main__':
